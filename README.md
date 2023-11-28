b2aa4d53d15f6471b1563e6a23c9a7cd4bb06837
Jason Wang <jasonw123451@gmail.com>
2023-11-27 16:48:32 -0800

    small updates

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-## CS32-Arcade
+# BlipBoy : CS 32 Arcade Project
+
+## What the project does, and why: 
+
+
+## How to build the project:
+
+
+## How to use the executables once they're built:
+
+
+## An overview of how the code is organized:
--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "CS32 Arcade", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
